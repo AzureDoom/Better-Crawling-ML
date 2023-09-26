4c2a3a29fa677407bb8287fab6faaa27198ffc52
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-26 08:50:19 -0400

    Apply to Vanilla Living Entity render for pre/post rendering

---
+++ Common/src/main/java/mod/azuredoom/bettercrawling/mixin/client/MixinLivingRenderer.java
@@ -0,0 +1,32 @@
+package mod.azuredoom.bettercrawling.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import mod.azuredoom.bettercrawling.Constants;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class MixinLivingRenderer <T extends LivingEntity, M extends EntityModel<T>> extends EntityRenderer<T> {
+
+    protected MixinLivingRenderer(EntityRendererProvider.Context context) {
+        super(context);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("HEAD"))
+    private void livingRenderPre(T livingEntity, float f, float partialTicks, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
+        Constants.onPreRenderLiving(livingEntity, partialTicks, poseStack);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("RETURN"))
+    private void livingRenderPost(T livingEntity, float f, float partialTicks, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
+        Constants.onPostRenderLiving(livingEntity, partialTicks, poseStack, multiBufferSource);
+    }
+}
